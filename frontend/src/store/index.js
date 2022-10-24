85542141055c2658f867b357d8c6c43dfe8270be
Roman <roman@pop-os.localdomain>
2022-10-24 19:44:23 +0200

    setup Vuex store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,8 +4,30 @@ export default createStore({
   state: {
   },
   getters: {
+    GetSpecificQuote: state => {
+      return state.quotes.filter(q => q._id == id)[0]
+    }
   },
   mutations: {
+    GetRandomQuote (state){
+      fetch("http://localhost:3000/quotes/random")
+      .then(res => res.json())
+      .then(data => {
+        if(state.quote._id != data._id){
+          state.quote = data;
+        }
+        else {
+          this.commit('GetRandomQuote');
+        }
+      })
+    },
+    GetAllQuotes(state){
+      fetch("http://localhost:3000/quotes")
+      .then(res => res.json())
+      .then(data => {
+        state.quotes = data;
+      });
+    }
   },
   actions: {
   },
